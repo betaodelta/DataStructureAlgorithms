288ffd542ef1d07c60b13de9239d6c6e3856f58a
betaodelta <prathameshvsarode1607@gmail.com>
2026-03-05 19:03:43 +0000

    5-3-26 JS code

---
+++ 1758. Minimum Changes To Make Alternating Binary String.js
@@ -0,0 +1,13 @@
+
+    let n = s.length;
+    let str1 = "" ,  str2 = "";
+    for(let i=0;i<n;i+=2){
+        str1 += "10";
+        str2 += "01";
+    }
+    let val1 = 0, val2 = 0;
+    for(let i=0;i<n;i++){
+        if(s[i]!==str1[i]) val1++;
+        if(s[i]!==str2[i]) val2++;
+    }
+    return Math.min(val1,val2);
